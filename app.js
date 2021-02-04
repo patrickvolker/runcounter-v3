b170b3849cf9ed739091d52a5f55a7e68e9898dc
Patrick Volker <pat@patrickvolker.com>
2021-02-04 14:48:04 -0500

    cleaned up run_total, added progress bar

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const icon = document.getElementById('icon');
 const message = document.getElementById('message');
 const lastRunDiv = document.getElementById('lastRunDiv');
 const milesRemainingDiv = document.getElementById('milesRemainingDiv');
+const progressBar = document.getElementById('progressBar');
+const percentageDiv = document.getElementById('percentageDiv');
 
 //font awesome
 const runIcon = `fas fa-running mx-2`;
@@ -19,111 +21,99 @@ message.innerHTML = 'submit';
 
 //datedif
 let today = new Date();
-let from_date = new Date('2021/12/31')
-let difference = from_date>today ? from_date-today : today-from_date
-let datedif = Math.floor(difference/(1000*3600*24));
+let from_date = new Date('2021/12/31');
+let difference = from_date > today ? from_date - today : today - from_date;
+let datedif = Math.floor(difference / (1000 * 3600 * 24));
 
-//need to learn how to do these routes with jawsDB (http://localhost:5000/stats)
-//api FETCH all <is this needed?
-fetch('http://localhost:5000/stats')
-.then( (response) => response.json() )
-.then( function(json) {
-// get the value out of the JSON
-let lastID = json[json.length -1];
-// console.log(lastID.id);
-let run_length = userMiles.value;
-let run_total = lastID.run_total;
-let run_date = Date();
-let stats = json;
-let getStats = function(stats){
-    console.log(stats);
-};
-getStats(stats);
-}
-);
+//api FETCH all stats
+fetch('http://runcounter-v3.herokuapp.com/stats')
+  .then((response) => response.json())
+  .then(function (json) {
+    // get the value out of the JSON
+    let lastID = json[json.length - 1];
+    let run_length = userMiles.value;
+    let run_total = lastID.run_total;
+    let run_date = Date();
+    let stats = json;
+    let getStats = function (stats) {
+      console.log(stats);
+    };
+    getStats(stats);
+  });
 
-
-                //api FETCH run_total
-fetch('http://localhost:5000/stats/run_total')
-.then( (response) => response.json() )
-.then( function(json) {
-    console.log(json);
-// get the value out of the JSON
-let runTotal = json[0];
-//push stats into DOM
-//set total
-let setTotal = function(runTotal){
-    bigTotal.innerHTML = runTotal;
-}
-setTotal(runTotal.run_total);
-//set milesRemaining
-let setMilesRemaining = function(runTotal){
-    milesRemaining.innerHTML = (2000 - runTotal);
-}
-setMilesRemaining(runTotal.run_total);
-//set reqDailyAvg
-let setReqDailyAvg = function(runTotal, datedif) {
-    let reqDaily = ((2000 - runTotal.run_total) / datedif);
-    let roundDown = reqDaily.toFixed(2);
-    reqDailyAvg.innerHTML = roundDown;
-}
-setReqDailyAvg(runTotal, datedif);
-});
-
+//api FETCH run_total
+fetch('http://runcounter-v3.herokuapp.com/stats/run_total')
+  .then((response) => response.json())
+  .then(function (json) {
+    // get the value out of the JSON
+    let runTotal = json[0];
+    //push stats into DOM
+    //set total
+    let setTotal = function (runTotal) {
+      bigTotal.innerHTML = runTotal;
+    };
+    setTotal(runTotal.run_total);
+    //set milesRemaining
+    let setMilesRemaining = function (runTotal) {
+      milesRemaining.innerHTML = 2000 - runTotal;
+    };
+    setMilesRemaining(runTotal.run_total);
+    //set reqDailyAvg
+    let setReqDailyAvg = function (runTotal, datedif) {
+      let reqDaily = (2000 - runTotal.run_total) / datedif;
+      let roundDown = reqDaily.toFixed(2);
+      reqDailyAvg.innerHTML = roundDown;
+    };
+    setReqDailyAvg(runTotal, datedif);
+    let setPercentage = function (bigTotal) {
+      let percentage =
+        (bigTotal.innerHTML / 2000 / Math.pow(10, -2)).toFixed(2) + '%';
+      progressBar.setAttribute('style', 'width: ' + percentage);
+      progressBar.setAttribute('aria-valuenow', percentage);
+      percentageDiv.innerHTML = percentage;
+    };
+    setPercentage(bigTotal);
+  });
 //date for mySQL
 let date = new Date();
 let year = date.getFullYear().toString();
-let month = (date.getMonth() + 1).toString().padStart(2, "0");
+let month = (date.getMonth() + 1).toString().padStart(2, '0');
 let day = date.getDate().toString();
-let dateString = (year+'-'+ month+'-'+ day);
+let dateString = year + '-' + month + '-' + day;
 
 //NEW RUN function
-const newRun = function() {
-let run_date = dateString;
-let run_length = parseFloat(lastRun.innerHTML);
-let total = parseFloat(bigTotal.innerHTML);
-let newTotal = (run_length + total);
-    fetch('http://localhost:5000/stats', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-        run_date: dateString,
-        run_length: run_length,
-        run_total: newTotal
-        }),
-    });
-updateDisplay();
+const newRun = function () {
+  let run_date = dateString;
+  let run_length = parseFloat(lastRun.innerHTML);
+  fetch('http://runcounter-v3.herokuapp.com/stats', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      run_date: dateString,
+      run_length: run_length,
+    }),
+  });
+  updateDisplay();
 };
 
-
-
-const updateDisplay = function() {
-    icon.className = heartIcon;
-    lastRunDiv.classList.remove('collapse');
-    lastRun.innerHTML = userMiles.value;
-    milesRemainingDiv.classList.remove('collapse');
-    milesRemaining.classList.remove('collapse');
-    reqDailyAvgDiv.classList.remove('collapse');
-    submit.classList.add('disabled');
+const updateDisplay = function () {
+  icon.className = heartIcon;
+  lastRun.innerHTML = userMiles.value;
 };
 
 //button switcher
-submit.addEventListener('click', function(){
-
-    if (userMiles.value == 0) {
-        message.innerHTML = 'maybe tomorrow?';
-    }
-    else if (userMiles.value >= 1 && userMiles.value <= 5) {
-        message.innerHTML = 'okok!';
-    }
-    else if (userMiles.value >= 6 && userMiles.value <= 9) {
-        message.innerHTML = 'respect!';
-    }
-    else if (userMiles.value >= 10) {
-        message.innerHTML = 'oh nice!!';
-    }
-    updateDisplay();
-    newRun();
+submit.addEventListener('click', function () {
+  if (userMiles.value == 0) {
+    message.innerHTML = 'maybe tomorrow?';
+  } else if (userMiles.value >= 1 && userMiles.value <= 5) {
+    message.innerHTML = 'okok!';
+  } else if (userMiles.value >= 6 && userMiles.value <= 9) {
+    message.innerHTML = 'respect!';
+  } else if (userMiles.value >= 10) {
+    message.innerHTML = 'oh nice!!';
+  }
+  newRun();
+  updateDisplay();
 });
--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 require('dotenv').config();
 const express = require('express');
-const mysql =  require('mysql');
+const mysql = require('mysql');
 const app = express();
 const cors = require('cors');
 const dbHost = process.env.dbHost;
 const dbUser = process.env.dbUser;
 const dbPassword = process.env.dbPassword;
 const dbJaws = process.env.dbJaws;
-const dbPort = (process.env.dbPort || 5000);
+const dbPort = process.env.dbPort;
+const PORT = process.env.PORT;
 
 app.use(cors());
 //jawsdb
-if (process.env.JAWSDB_URL){
-    var db = mysql.createConnection(process.env.JAWSDB_URL);
+if (process.env.JAWSDB_URL) {
+  var db = mysql.createConnection(process.env.JAWSDB_URL);
 }
 //or local connection
 // var db = mysql.createConnection({
@@ -23,54 +24,59 @@ if (process.env.JAWSDB_URL){
 //     port: (dbPort || 5000)
 // });
 
-app.use(express.urlencoded({
-    extended: true
-})
-)
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 app.use(express.json());
 
 //connect
 db.connect((err) => {
-    if(err){
-        throw err;
-    }
-    console.log('MySQL connected!');
+  if (err) {
+    throw err;
+  }
+  console.log('MySQL connected!');
 });
 
 //need to learn how to do these routes with jawsDB (or just anything) (http://localhost:5000/stats)
 app.get('/stats', (req, res) => {
-    const sql = 'SELECT * FROM `stats`';
-    db.query(sql, (err, result) => {
-        if (err) throw 'Something bad happened...sorry...';
-        res.send(result);
-    });
+  const sql = 'SELECT * FROM `stats`';
+  db.query(sql, (err, result) => {
+    if (err) throw 'Something bad happened...sorry...';
+    res.send(result);
+  });
 });
 
 app.get('/stats/run_total', (req, res) => {
-    const sql = 'SELECT run_total FROM `stats` WHERE run_total ORDER BY id DESC LIMIT 1';
-    db.query(sql, (err, result) => {
-        if (err) throw 'Something bad happened...sorry...';
-        res.send(result);
-    });
+  const sql = 'SELECT SUM(run_length) FROM `stats`';
+  db.query(sql, (err, result) => {
+    if (err) throw 'Something bad happened...sorry...';
+    res.send(result);
+  });
 });
 
 app.post('/stats', (req, res) => {
-    //get values from req
-    let run_date = req.body.run_date;
-    let run_length = req.body.run_length;
-    let run_total = req.body.run_total;
-       //plug values into sql
-       const sql = 'INSERT INTO `stats` (run_date, run_length, run_total) VALUES ("' + run_date + '", ' + run_length + ', ' + run_total + ')';
-        console.log(sql);
-        db.query(sql, (err, result) => {
-        if (err) throw 'Something bad happened...sorry...';
-        res.send(result);
-        console.log('Run posted!');
-    });
+  //get values from req
+  let run_date = req.body.run_date;
+  let run_length = req.body.run_length;
+  //plug values into sql
+  const sql =
+    'INSERT INTO `stats` (run_date, run_length) VALUES ("' +
+    run_date +
+    '", ' +
+    run_length +
+    ')';
+  console.log(sql);
+  db.query(sql, (err, result) => {
+    if (err) throw 'Something bad happened...sorry...';
+    res.send(result);
+    console.log('Run posted!');
+  });
 });
 
-app.listen(5000, () => console.log('Online!'));
-
+app.listen(process.env.PORT || process.env.dbPort, () =>
+  console.log('Online!')
+);
 
 //sql line for inserting new run ---> INSERT INTO `stats` (`id`, `run_date`, `run_length`, `run_total`) VALUES (NULL, '2021-01-08', '6', '48');
-//sql line for just getting most recent run_total 'SELECT run_total FROM stats WHERE run_total ORDER BY id DESC LIMIT 1'
